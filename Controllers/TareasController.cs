6c1218f47822e3b70e52c4f5da75dc5bbb4aba6d
¨Smailyn <sceballov@gmail.com>
2025-07-26 19:20:26 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationLayer.Services.TaskService;
+using DomainLayer.DTO;
+using DomainLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly TaskService _service;
+        public TareasController(TaskService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<Tareas>>> GetTaskAllAsync()
+            => await _service.GetTaskAllAsync();
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Response<Tareas>>> GetTaskByIdAllAsync(int id)
+            => await _service.GetTaskByIdAllAsync(id);
+
+        [HttpPost]
+        public async Task<ActionResult<Response<string>>> AddTaskAllAsync(Tareas tarea)
+            => await _service.AddTaskAllAsync(tarea);
+
+        [HttpPut]
+        public async Task<ActionResult<Response<string>>> UpdateTaskAllAsync(Tareas tarea)
+           => await _service.UpdateTaskAllAsync(tarea);
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Response<string>>> DeleteTaskAllAsync(int id)
+            => await _service.DeleteTaskAllAsync(id);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using ApplicationLayer.Services.TaskService;
+using DomainLayer.Models;
+using InfrastructureLayer.Context;
+using InfrastructureLayer.Repositorio.Commons;
+using InfrastructureLayer.Repositorio.TaskRepository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<TaskManagerContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TaskManagerDB"));
+});
+
+builder.Services.AddScoped<ICommonsProccess<Tareas>, TaskRepository>();
+builder.Services.AddScoped<TaskService>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var Context = scope.ServiceProvider.GetRequiredService<TaskManagerContext>();
+//    Context.Database.Migrate(); 
+//}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
